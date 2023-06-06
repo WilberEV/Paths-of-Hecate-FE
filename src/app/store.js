c5ad1db8bdcf089391e45779062d53482bd94c58
WilberEV <wilberesparragoza@gmail.com>
2023-06-06 21:01:29 +0200

    userslice + Header initial layout

---
+++ src/app/store.js
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from '../layouts/userSlice';
+
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+    user: userSlice,
+    detail: detailSlice
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+    reducer: persistedReducer,
+    middleware: [thunk]
+});
