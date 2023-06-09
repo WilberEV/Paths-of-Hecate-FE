40cd81a4091cc1f981592775c439c995edd4c527
WilberEV <wilberesparragoza@gmail.com>
2023-06-09 20:45:50 +0200

    Header: Change on header style

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -25,34 +25,39 @@ export const Header = () => {
     <div>
       {!datosUserRedux?.credentials?.token ? (
         <div>
-          <Navbar bg="dark" variant="dark">
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
               <Navbar.Brand href="/">Paths of Hecate</Navbar.Brand>
-              <Nav className="me-auto">
-
-              </Nav>
-              <Nav>
-                <Nav.Link href="login">Login</Nav.Link>
-                <Nav.Link href="signup">Sign Up</Nav.Link>
-              </Nav>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
+                </Nav>
+                <Nav>
+                  <Nav.Link href="login">Login</Nav.Link>
+                  <Nav.Link href="signup">Sign Up</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </div>
       ) : (
-            <Navbar bg="dark" variant="dark">
-              <Container>
-                <Navbar.Brand href="/">Paths of Hecate</Navbar.Brand>
-                <Nav className="me-auto">
-                  <Nav.Link href="profile">Profile</Nav.Link>
-                </Nav>
-                <Nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Paths of Hecate</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="profile">Profile</Nav.Link>
+              </Nav>
+              <Nav>
                 {datosUserRedux?.credentials?.user.role === "ADMIN" && (
-                    <Nav.Link href="admin">Admin</Nav.Link>
-                  )}
-                  <Nav.Link onClick={() => logMeOut()}>Log Out</Nav.Link>
-                </Nav>
-              </Container>
-            </Navbar>
+                  <Nav.Link href="admin">Admin</Nav.Link>
+                )}
+                <Nav.Link onClick={() => logMeOut()}>Log Out</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       )}
     </div>
   );
